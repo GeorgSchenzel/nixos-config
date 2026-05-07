fb652c88483b0ab8a5dcc9f0fafb5b8e699603e2
Georg Schenzel <schenzel.georg@gmail.com>
2026-05-07 20:49:24 +0000

    fix audio device defaults using WirePlumber 0.5 node.name matching

--- modules/programs/desktop-environment [Nn]/home.nix
+++ modules/programs/desktop-environment [Nn]/home.nix
@@ -131,6 +131,11 @@
             "${modifier}+a" = "focus parent";
 
             "${modifier}+r" = "mode resize";
+
+            "XF86AudioRaiseVolume" = "exec wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+";
+            "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+            "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+            "XF86AudioMicMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
           };
 
           modes.resize = {
---
+++ modules/services/audio [N]/audio.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  flake.modules.nixos.audio = { pkgs, ... }: {
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    environment.systemPackages = [ pkgs.pavucontrol ];
+  };
+}
