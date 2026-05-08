e6c7fbdc90364ff27d471448b7e0a398415dd045
Georg Schenzel <schenzel.georg@gmail.com>
2026-05-08 13:57:43 +0000

    minimal den setup

--- flake.nix
+++ flake.nix
@@ -1,36 +1,20 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    disko = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/disko";
-    };
+    den.url = "github:vic/den";
     flake-file.url = "github:vic/flake-file";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-25.11";
     };
-    impermanence.url = "github:nix-community/impermanence";
     import-tree.url = "github:vic/import-tree";
-    mac-app-util.url = "github:hraban/mac-app-util";
-    nix-darwin = {
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
-    };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    opencode.url = "github:anomalyco/opencode";
-    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
-    sops-nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:Mic92/sops-nix";
-    };
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
   };
-
 }
---
+++ modules/georg.nix
@@ -0,0 +1,21 @@
+{ den, ... }:
+{
+  # user aspect
+  den.aspects.georg = {
+    includes = [
+      den.provides.define-user
+      den.provides.primary-user
+      (den.provides.user-shell "bash")
+    ];
+
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.htop ];
+      };
+
+    # user can provide NixOS configurations
+    # to any host it is included on
+    provides.to-hosts.nixos = { pkgs, ... }: { };
+  };
+}
